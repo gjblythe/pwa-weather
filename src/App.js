20c8737bc02588ae071b6918766be1e42927bfb9
Garrett Blythe <gblythe@aspen-systems.com>
2020-12-03 14:09:02 -0700

    before moving to service workers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import {AppBar, Button, Card, CardContent, CardHeader, Container, Grid, makeStyles, TextField, Toolbar, Typography} from '@material-ui/core';
+import React, {useState} from 'react';
+import {fetchWeather} from './api/fetchWeather';
 import './App.css';
 
-function App() {
+const useStyles = makeStyles((theme) => ({
+  search: {
+    margin: theme.spacing(2),
+  },
+  button: {
+    margin: theme.spacing(1),
+  },
+  card: {
+    width: 400,
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+ 
+  const search = async () => {
+    const data = await fetchWeather(query);
+    console.log(data)
+    setWeather(data);
+    setQuery('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position={'static'}>
+        <Toolbar>
+          <Typography>PWA Weather App</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Container maxWidth={'md'}>
+          <Grid container={true} className={classes.search} alignContent={'center'}>
+            <Grid item={true} md={10}>
+              <TextField 
+                id={'city'}
+                label={'Location(City)'}
+                variant={'outlined'} 
+                fullWidth={true}
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </Grid>
+            <Grid item={true} md={2}>
+              <Button className={classes.button} variant={'contained'} color={'primary'} onClick={search}>Submit</Button> 
+            </Grid>
+          </Grid>
+          {weather.main && (
+            <Grid container={true} justify={'center'}>
+              <Card className={classes.card}>
+                <CardHeader title={weather.name}/>
+                <CardContent >
+                  <Typography variant={'caption'}>Temp</Typography>
+                  <Typography variant={'h4'}>{Math.round(weather.main.temp)}<sup>&deg;F</sup></Typography>
+                  <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description}/>
+                  <Typography variant={'subtitle2'}>{weather.weather[0].description.toUpperCase()}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          )}
+        </Container>
+      </main>
     </div>
   );
-}
-
-export default App;
+};
---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '737e3beb31320b02a342ef6805f38a2f';
+
+export const fetchWeather = async (query) => {
+  const {data} = await axios.get(URL, {
+    params: {
+      q: query,
+      units: 'imperial',
+      appid: API_KEY,
+    }
+    
+  });
+  return data;
+}
