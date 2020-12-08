9d18ac3abdd6e4c243ea029eede5d8f6a6067329
Garrett Blythe <gblythe@aspen-systems.com>
2020-12-08 08:06:14 -0700

    update complete

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from '../../../pwa-weather/node_modules/axios';
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '737e3beb31320b02a342ef6805f38a2f';
 
 export const fetchWeather = async (query) => {
   const {data} = await axios.get(URL, {
